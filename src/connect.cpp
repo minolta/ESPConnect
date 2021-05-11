0a14fb74ebc71baa70a4f6ce44045cb4589e82be
Kritchai Phromros <ky@pixka.me>
2021-05-11 18:00:00 +0700

    Add ap mode

--- src/connect.cpp
+++ src/connect.cpp
@@ -36,10 +36,14 @@ int ConnectObj::checkwifi(const char *p)
 boolean ConnectObj::apmode(char *ssid, char *password)
 {
     WiFi.softAPConfig(IPAddress(192, 168, 5, 1), IPAddress(192, 168, 5, 1), IPAddress(255, 255, 255, 0));
-    WiFi.softAP(ssid, password);
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
+    if (WiFi.softAP(ssid, password))
+    {
+        IPAddress IP = WiFi.softAPIP();
+        Serial.print("AP IP address: ");
+        Serial.println(IP);
+        return true;
+    }
+    return false;
 }
 void ConnectObj::connect()
 {
